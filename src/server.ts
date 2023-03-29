4b7e86bbf20cb51e641b47d8739ad94253286cb3
Dimitar Iliev <dimitar.iliev.github@gmail.com>
2023-03-29 08:59:47 +0200

    feat: server support for cors and urlencoded (flutter) request body

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import posts from './api/posts'
 import auth from './api/auth'
 import fields from "./api/fields";
 import postConfigs from './api/post_config'
-import {json} from "body-parser"
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(json());
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/users", user);
 app.use('/posts', posts);
