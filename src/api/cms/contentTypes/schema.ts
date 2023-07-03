b648a393d15acb9cc2930c060c501a0d7c220085
Dimitar Iliev <dimitar.iliev.github@gmail.com>
2023-07-03 08:43:55 +0200

    refactor(schema): content type schema uses definitions

--- src/api/cms/contentTypes/schema.ts
+++ src/api/cms/contentTypes/schema.ts
@@ -1,132 +1,104 @@
-import { JSONSchemaType } from "ajv";
-import { StoryConfigData } from "~/models/contentType";
-import ajv from "~/schema/core";
+import { JSONSchemaType } from "ajv"
+import { ContentType } from "~/models/contentType"
+import ajv from "~/schema/core"
 
-const featureRefUrl = "http://plenty-cms/schemas/defs.json";
+// const featureRefUrl = "http://plenty-cms/schemas/defs.json"
 
-const featureDef = {
-  $id: featureRefUrl,
+// const featureDef = {
+//   $id: featureRefUrl,
+//   definitions: {
+//     feature: {
+//       type: "object",
+//       properties: {
+//         enabled: {
+//           type: "boolean",
+//         },
+//       },
+//     },
+//   },
+// }
+
+// ajv.addSchema(featureDef)
+
+ajv.addSchema({
+  type: "object",
+  $id: "/schemas/defs.json",
   definitions: {
-    feature: {
+    // defs.json#/definitions/component
+    component: {
       type: "object",
       properties: {
-        enabled: {
-          type: "boolean",
+        groupName: {
+          type: "string",
         },
-      },
-    },
-  },
-};
-
-ajv.addSchema(featureDef);
-
-const validate = ajv.compile<JSONSchemaType<StoryConfigData>>({
-  type: "object",
-  $async: true,
-  properties: {
-    fields: {
-      type: "array",
-      minItems: 0,
-      items:
-      {
-        type: "object",
-        properties: {
-          groupName: {
-            type: "string",
-          },
-          rows: {
-            type: "array",
-            minItems: 0,
-            items:
-            {
-              type: "object",
-              properties: {
-                label: {
-                  type: "string",
-                },
-                displayName: {
-                  type: "string",
-                },
-                type: {
-                  enum: ["custom", "text", "number", "boolean"],
-                },
-                width: {
-                  type: ["string", "number"],
-                },
-                source: {
-                  // anyOf: [
-                  //   {
-                  //     type: "object",
-                  //     properties: {
-                  //       type: {
-                  //         type: "string"
-                  //       },
-                  //       options: {
-                  //         type: {
-                  //           type: "string"
-                  //         },
-                  //         items: {
-                  //           type: "string"
-                  //         }
-                  //       }
-                  //     }
-                  //   },
-                  // {
-                  //   type: "object",
-                  //   properties: {
-                  //     type: {
-                  //       type: "string"
-                  //     },
-                  //     dataSourceId: {
-                  //       type: "string"
-                  //     }
-                  //   }
-                  // },
-                  // {
-                  //   type: "object",
-                  //   properties: {
-                  //     type: {
-                  //       type: "string"
-                  //     },
-                  //     url: {
-                  //       type: "string",
-                  //       format: "url"
-                  //     }
-                  //   }
-                  // }
-                  // ]
-                },
+        rows: {
+          type: "array",
+          minItems: 0,
+          items: {
+            type: "object",
+            properties: {
+              label: {
+                type: "string",
               },
+              displayName: {
+                type: "string",
+              },
+              type: {
+                enum: ["custom", "text", "number", "boolean"],
+              },
+              width: {
+                type: ["string", "number"],
+              },
+              source: {},
             },
           },
         },
       },
     },
+  },
+})
+
+const validate = ajv.compile<JSONSchemaType<ContentType>>({
+  type: "object",
+  $id: "/schemas/content.json",
+  $async: true,
+  properties: {
+    fields: {
+      type: "array",
+      minItems: 0,
+      items: {
+        $ref: "defs.json#/definitions/component"
+      },
+    },
     name: {
       type: "string",
     },
-    tags: {},
-    features: {
-      type: ["object", "null"],
-      properties: {
-        likes: {
-          $ref: `${featureRefUrl}#/definitions/feature`,
-        },
-        comments: {
-          $ref: `${featureRefUrl}#/definitions/feature`,
-        },
-        rating: {
-          $ref: `${featureRefUrl}#/definitions/feature`,
-        },
-      },
-    },
     slug: {
       type: "string",
-      found: {notIn: true, in: "storiesConfig:slug"}
-    }
+      found: { notIn: true, in: "storiesConfig:slug" },
+    },
+    tags: {},
+    features: {}
   },
+  // optionalProperties: {
+    // tags: {},
+    // features: {
+    //   type: ["object"],
+    //   properties: {
+    //     likes: {
+    //       $ref: `${featureRefUrl}#/definitions/feature`,
+    //     },
+    //     comments: {
+    //       $ref: `${featureRefUrl}#/definitions/feature`,
+    //     },
+    //     rating: {
+    //       $ref: `${featureRefUrl}#/definitions/feature`,
+    //     },
+    //   },
+    // },
+  // },
   required: ["fields", "slug", "name"],
-  additionalProperties: false
+  additionalProperties: false,
 })
 
-export default validate;
+export default validate
