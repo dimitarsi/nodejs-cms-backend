7b57ead0993b747a288585625fe7de900c215463
Dimitar Iliev <dimitar.iliev.github@gmail.com>
2023-10-28 08:55:54 +0200

    feat: improve and test router proxy

--- src/core/api/router.ts
+++ src/core/api/router.ts
@@ -15,53 +15,65 @@ export function registerMiddleware(
   })
 }
 
-export default function Router() {
+export default function Router(prefix: string = "") {
   const router = ExpressRouter()
+  type RouterType = typeof router
   const registeredMiddlewares: string[] = []
-  const routerProxy: typeof router = new Proxy(router, {
-    get(target, p) {
-      if (p === "use") {
-        // @ts-ignore
-        return (...args) => {
-          const rest = args.slice(0, -1)
-          const lastArg = args.slice(-1)
-          const middleware =
-            lastArg.length &&
-            typeof lastArg[0] === "string" &&
-            middlewares.find((m) => m.name === lastArg[0])?.handler
-
-          if (middleware) {
-            registeredMiddlewares.push(middleware.name)
-          }
-
+  const proxyRouterFactory = (router: RouterType) =>
+    new Proxy(router, {
+      get(target, p) {
+        if (p === "use") {
           // @ts-ignore
-          return target[p].apply(
-            target,
+          return (...args) => {
+            const rest = args.slice(0, -1)
+            const lastArg = args.slice(-1)
+            const middleware =
+              lastArg.length &&
+              typeof lastArg[0] === "string" &&
+              middlewares.find((m) => m.name === lastArg[0])?.handler
+
+            if (middleware) {
+              registeredMiddlewares.push(middleware.name)
+            }
+
             // @ts-ignore
-            middleware ? [...rest, middleware] : args
-          )
+            return target[p].apply(
+              target,
+              // @ts-ignore
+              middleware ? [...rest, middleware] : args
+            )
+          }
         }
-      }
 
-      if (spiedProperties.includes(p.toString())) {
-        // @ts-ignore
-        return (...args) => {
-          textRouteReporter(`${p.toString()}`, registeredMiddlewares, args)
+        if (spiedProperties.includes(p.toString())) {
           // @ts-ignore
-          return target[p].apply(target, args)
+          return (...args) => {
+            textRouteReporter(
+              `${p.toString()}`,
+              prefix,
+              registeredMiddlewares,
+              args
+            )
+            // @ts-ignore
+            return target[p].apply(target, args)
+          }
         }
-      }
 
-      // @ts-ignore
-      return target[p]
-    },
-    // Each time a router handles a request this function is called
-    async apply(target, _thisArg, argArray) {
-      // @ts-ignore
-      const result = await target(...argArray)
-      return result
-    },
-  })
+        // @ts-ignore
+        return target[p]
+      },
+      // Each time a router handles a request this function is called
+      async apply(target, _thisArg, argArray) {
+        // @ts-ignore
+        const result = await target(...argArray)
+        return result
+      },
+    })
 
-  return routerProxy
+  const routerProxy: typeof router = proxyRouterFactory(router)
+
+  const rootRouter = ExpressRouter()
+  rootRouter.use(prefix, routerProxy)
+
+  return proxyRouterFactory(rootRouter)
 }
