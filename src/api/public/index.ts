9d49d069bdd1d7f9fee23015dd49dbc0e6eaca7f
Dimitar Iliev <dimitar.iliev.github@gmail.com>
2023-11-11 09:13:59 +0100

    chore: cleanup public access

--- src/api/public/index.ts
+++ src/api/public/index.ts
@@ -1,9 +1,5 @@
-// import { handler } from "./../../middleware/auth"
 import auth from "./auth"
-import Router from "~/core/api/router"
-import media from "./media"
-import revokeAccessTokens from "./revokeAccessTokens"
-import fastify, { FastifyInstance } from "fastify"
+import { FastifyInstance } from "fastify"
 
 export default function publicApi(
   instance: FastifyInstance,
@@ -12,16 +8,4 @@ export default function publicApi(
 ) {
   instance.register(auth)
   done()
-  // instance.register(media)
-
-  // auth(router)
-  // router.get("/media/:hash", (req, reply) => {
-  //   mediaHandler(req, reply)
-  // })
-
-  // const devRouter = Router()
-  // // devRouter.use("devOnlyEnv")
-  // revokeAccessTokens(devRouter)
-
-  // router.use(devRouter)
 }
